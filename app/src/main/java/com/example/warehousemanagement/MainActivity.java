29a790dc9a34bc8e83535e46c793c6c6565cfc10
Lưu Tiến Phát <luutienphat@gmail.com>
2022-03-23 20:55:24 +0700

    Add private methods set control and event

--- app/src/main/java/com/example/warehousemanagement/MainActivity.java
+++ app/src/main/java/com/example/warehousemanagement/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.warehousemanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnWarehouse, btnProduct, btnReceipt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +20,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setEvent() {
+        btnProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleBtnProductClick(view);
+            }
+        });
+
+        btnReceipt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleBtnReceiptClick(view);
+            }
+        });
+
+        btnWarehouse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleBtnWarehouseClick(view);
+            }
+        });
     }
 
     private void setControl() {
+        btnProduct = findViewById(R.id.receipt_btn);
+        btnWarehouse = findViewById(R.id.warehouse_btn);
+        btnReceipt = findViewById(R.id.receipt_btn);
+    }
+
+    public void handleBtnWarehouseClick(View view) {
+        Intent intent = new Intent(this, WarehouseActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleBtnProductClick(View view) {
+        Intent intent = new Intent(this, WarehouseActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleBtnReceiptClick(View view) {
+        Intent intent = new Intent(this, WarehouseActivity.class);
+        startActivity(intent);
     }
 }
