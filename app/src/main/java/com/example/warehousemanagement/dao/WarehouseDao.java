4a08a138b406330b8b2f97a4a82f288bbe6abd7e
Lưu Tiến Phát <luutienphat@gmail.com>
2022-03-25 23:50:10 +0700

    Add update and delete methods to the interface

--- app/src/main/java/com/example/warehousemanagement/dao/WarehouseDao.java
+++ app/src/main/java/com/example/warehousemanagement/dao/WarehouseDao.java
@@ -1,8 +1,10 @@
 package com.example.warehousemanagement.dao;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import com.example.warehousemanagement.entity.WarehouseEntity;
 
@@ -12,6 +14,12 @@ import java.util.List;
 public interface WarehouseDao {
     @Insert
     void insertOne(WarehouseEntity warehouseEntity);
+
+    @Update
+    void updateOne(WarehouseEntity warehouse);
+
+    @Delete
+    void deleteOne(WarehouseEntity warehouse);
 
     @Query("SELECT * FROM warehouse")
     List<WarehouseEntity> getAll();
