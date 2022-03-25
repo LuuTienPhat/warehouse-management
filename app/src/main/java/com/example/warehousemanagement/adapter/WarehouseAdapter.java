34d9e1823d35bfa8adc3eefb7177739f12826ebe
Lưu Tiến Phát <luutienphat@gmail.com>
2022-03-25 23:47:33 +0700

    Remove default constructors and replace them with the self created one.
    Modify the getView method

--- app/src/main/java/com/example/warehousemanagement/adapter/WarehouseAdapter.java
+++ app/src/main/java/com/example/warehousemanagement/adapter/WarehouseAdapter.java
@@ -5,61 +5,47 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.warehousemanagement.R;
-import com.example.warehousemanagement.model.Warehouse;
+import com.example.warehousemanagement.entity.WarehouseEntity;
 
 import java.util.List;
 
-public class WarehouseAdapter extends ArrayAdapter<Warehouse> {
+public class WarehouseAdapter extends ArrayAdapter<WarehouseEntity> {
 
-    public WarehouseAdapter(@NonNull Context context, int resource) {
-        super(context, resource);
+    Context context;
+    List<WarehouseEntity> warehouses;
+    int resource;
+
+    public WarehouseAdapter(@NonNull Context context, int resource, @NonNull List<WarehouseEntity> warehouses) {
+        super(context, resource, warehouses);
+        this.context = context;
+        this.resource = resource;
+        this.warehouses = warehouses;
     }
 
-    public WarehouseAdapter(@NonNull Context context, int resource, int textViewResourceId) {
-        super(context, resource, textViewResourceId);
-    }
-
-    public WarehouseAdapter(@NonNull Context context, int resource, @NonNull Warehouse[] objects) {
-        super(context, resource, objects);
-    }
-
-    public WarehouseAdapter(@NonNull Context context, int resource, int textViewResourceId, @NonNull Warehouse[] objects) {
-        super(context, resource, textViewResourceId, objects);
-    }
-
-    public WarehouseAdapter(@NonNull Context context, int resource, @NonNull List<Warehouse> objects) {
-        super(context, resource, objects);
-    }
-
-    public WarehouseAdapter(@NonNull Context context, int resource, int textViewResourceId, @NonNull List<Warehouse> objects) {
-        super(context, resource, textViewResourceId, objects);
-    }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        Warehouse warehouse = getItem(position);
+        WarehouseEntity warehouse = getItem(position);
 
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.warehouse_item, parent, false);
-
         }
 
-        TextView tvWarehouseId = convertView.findViewById(R.id.warehouse_id);
-        TextView tvWarehouseName = convertView.findViewById(R.id.warehouse_name);
-        TextView tvWarehouseAddress = convertView.findViewById(R.id.warehouse_address);
+        TextView tvWarehouseId = convertView.findViewById(R.id.warehouseId);
+        TextView tvWarehouseName = convertView.findViewById(R.id.warehouseName);
+        TextView tvWarehouseAddress = convertView.findViewById(R.id.warehouseAddress);
 
         tvWarehouseId.setText(warehouse.getId());
         tvWarehouseName.setText(warehouse.getName());
         tvWarehouseAddress.setText(warehouse.getAddress());
 
-        return super.getView(position, convertView, parent);
+        return convertView;
     }
 }
