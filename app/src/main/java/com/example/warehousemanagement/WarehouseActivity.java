33bba9784c27b28f44f2da6f8c633c3e53cb01e9
Lưu Tiến Phát <luutienphat@gmail.com>
2022-03-26 00:24:23 +0700

    Mapping all the missing views.
    Access the db then read data from it and display them in the list view.
    
    Set click event for each list view item

--- app/src/main/java/com/example/warehousemanagement/WarehouseActivity.java
+++ app/src/main/java/com/example/warehousemanagement/WarehouseActivity.java
@@ -1,33 +1,68 @@
 package com.example.warehousemanagement;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.warehousemanagement.adapter.WarehouseAdapter;
+import com.example.warehousemanagement.dao.WarehouseDao;
+import com.example.warehousemanagement.entity.WarehouseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WarehouseActivity extends AppCompatActivity {
     ImageButton btnAdd, btnMinimize;
     SearchView searchView;
+    TextView tvTitle;
     ListView listview;
+    WarehouseAdapter warehouseAdapter;
+    List<WarehouseEntity> warehouses = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_warehouse);
+        setContentView(R.layout.activity_view);
         setControl();
         setEvent();
+
+        tvTitle.setText("Kho");
+
+        AppDatabase db = AppDatabase.getInstance(this);
+        WarehouseDao warehouseDao = db.warehouseDao();
+        warehouses = warehouseDao.getAll();
+
+        WarehouseAdapter warehouseAdapter = new WarehouseAdapter(this, R.layout.warehouse_item, warehouses);
+        listview.setAdapter(warehouseAdapter);
     }
 
     private void setControl() {
-        btnAdd = findViewById(R.id.add_btn);
-        btnMinimize = findViewById(R.id.minimize_btn);
-
+        btnAdd = findViewById(R.id.btnAdd);
+        btnMinimize = findViewById(R.id.btnMinimize);
+        listview = findViewById(R.id.listView);
+        tvTitle = findViewById(R.id.tvTitle);
     }
 
     private void setEvent() {
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                WarehouseEntity warehouse = (WarehouseEntity) adapterView.getItemAtPosition(position);
 
+                Intent intent = new Intent(WarehouseActivity.this, WarehouseDetailActivity.class);
+                intent.putExtra("warehouseId", warehouse.getId());
+                intent.putExtra("warehouseName", warehouse.getName());
+                intent.putExtra("warehouseAddress", warehouse.getAddress());
+                startActivity(intent);
+            }
+        });
     }
+
 }
